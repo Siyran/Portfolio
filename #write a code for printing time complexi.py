da85ef9156de190cfa1a4cc4f24df4a2f9fc144d
Abrar Ahmad <118918239+Siyran@users.noreply.github.com>
2026-04-10 10:30:04 +0530

    Update portfolio content

---
+++ #write a code for printing time complexi.py
@@ -0,0 +1,9 @@
+#write a code for printing time complexity of a fibnocci series
+
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
